d87fcc25f844f12ac051027acb06eea5e6dcb082
Sophie <141583731+studiostroeve@users.noreply.github.com>
2025-05-13 14:01:50 +0200

    update
    Mobile navbar support added

--- navbar.js
+++ navbar.js
@@ -4,7 +4,9 @@ const links = document.getElementById("links")
 burger.addEventListener("click", () => {
     if (links.classList.contains("active")) {
         links.classList.remove("active")
+        burger.classList.remove("active")
     } else {
         links.classList.add("active")
+        burger.classList.add("active")
     }
 });
