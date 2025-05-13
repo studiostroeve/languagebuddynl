7ea32248102f9aaba508a29badf0d82136995699
Sophie <141583731+studiostroeve@users.noreply.github.com>
2025-05-13 13:41:58 +0200

    Update
    Mobile support development was started
    texts updated

---
+++ navbar.js
@@ -0,0 +1,10 @@
+const burger = document.getElementById("menu-burger")
+const links = document.getElementById("links")
+
+burger.addEventListener("click", () => {
+    if (links.classList.contains("active")) {
+        links.classList.remove("active")
+    } else {
+        links.classList.add("active")
+    }
+});
